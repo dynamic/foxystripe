2d9a6c4bfefc720a71276ccd9efe09421f8f607d
Chris Bryer <cjbryer@gmail.com>
2011-06-10 21:42:03 -0700

    fixed error from Product Page Preview Image

--- Code/ProductPage.php
+++ Code/ProductPage.php
@@ -203,7 +203,7 @@ class ProductPage extends Page {
 		$form .= $this->hiddenTag('category',$this->Category()->Code);
 		$form .= $this->hiddenTag('code', $this->Code);
 		$form .= $this->hiddenTag('price', $this->Price);
-		if($img = $this->PreviewImage()) $form .= $this->hiddenTag('image', $img->PaddedImage(80,80)->absoluteURL);
+		if($this->PreviewImage()->Exists()) $form .= $this->hiddenTag('image', $this->PreviewImage()->PaddedImage(80,80)->absoluteURL);
 		return $form;
 	}
 	
