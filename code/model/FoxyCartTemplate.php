f7893161001c2b92ea93f90307c8e3edff214afd
Jason Irish <jirish@dynamicdoes.com>
2013-06-15 16:45:07 -0500

    requirements for FoxyCartTemplate.php

--- code/model/FoxyCartTemplate.php
+++ code/model/FoxyCartTemplate.php
@@ -24,9 +24,10 @@ class FoxyCartTemplate_Controller extends ContentController {
 		// variables for file paths
 		$BaseHref = Director::absoluteBaseURL();
 		$ThemeDir = 'themes/' . SSViewer::current_theme() . '/';
-	
-		Requirements::css('https://' . FoxyCart::$foxyCartStoreName . '.foxycart.com/themes/text/styles.css" type="text/css');
-		//Requirements::css('https://' . FoxyCart::$foxyCartStoreName . '.foxycart.com/themes/standard/styles.css" type="text/css');
+		
+		Requirements::css('https://static.foxycart.com/scripts/colorbox/1.3.16/style1_fc/colorbox.css');
+		Requirements::css('https://' . FoxyCart::$foxyCartStoreName . '.foxycart.com/themes/standard/styles.css" type="text/css');
+		//Requirements::css('https://' . FoxyCart::$foxyCartStoreName . '.foxycart.com/themes/text/styles.css" type="text/css');
 		Requirements::css($BaseHref . $ThemeDir . 'css/foxycart.css');
 		
 		parent::init();
