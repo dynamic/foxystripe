ee1c231c2ae83717d053a7f0ca7efb0489a60586
Jason Irish <jirish@dynamicdoes.com>
2014-04-10 17:07:25 -0500

    implement initial order history
    fixes #5
    
    shipping first and last name in FoxyCartDatafeedCollector
    
    Customer data from FC datafeed
    
    fix for adding products to order
    
    FoxyCartDatafeedCollector - associate ProductPage with Order
    
    Member onBeforeWrite - set PasswordEncryption to sha1_2.4
    
    member onAfterWrite - setPassword = false
    
    fixed customer extension
    
    Member onAfterWrite
    
    set password_encryption_algorithm
    
    hash work
    
    custom passwordencryptor
    
    add email to check variable values
    
    hash work
    
    trying to emulate FoxyCart password hash
    
    set password encryption type
    
    fixed typo
    
    removed PasswordHash type
    
    escaped customer data
    
    customer sync from datafeed
    
    initial work on transaction datafeed parsing

---
+++ code/extensions/CustomerExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+class CustomerExtension extends DataExtension{
+
+    private static $db = array(
+        'Customer_ID' => 'Int',
+        'MinifraudScore' => 'Varchar',
+        'CustomerCompany' => 'Varchar',
+        'CustomerAddress1' => 'Varchar(200)',
+        'CustomerAddress2' => 'Varchar(200)',
+        'CustomerCity' => 'Varchar(100)',
+        'CustomerState' => 'Varchar(100)',
+        'CustomerPostalCode' => 'Varchar(10)',
+        'CustomerCountry' => 'Varchar(100)',
+        'CustomerPhone' => 'Varchar(20)',
+        'CustomerIP' => 'Varchar(20)',
+        'ShippingFirstName' => 'Varchar(100)',
+        'ShippingLastName' => 'Varchar(100)',
+        'ShippingCompany' => 'Varchar(100)',
+        'ShippingAddress1' => 'Varchar(200)',
+        'ShippingAddress2' => 'Varchar(200)',
+        'ShippingCity' => 'Varchar(100)',
+        'ShippingState' => 'Varchar(100)',
+        'ShippingPostalCode' => 'Varchar(10)',
+        'ShippingCountry' => 'Varchar(100)',
+        'ShippingPhone' => 'Varchar(20)'
+    );
+
+    public function onBeforeWrite() {
+        parent::onBeforeWrite();
+
+        $this->owner->PasswordEncryption = 'sha1_v2.4';
+    }
+
+}
---
+++ code/objects/Order.php
@@ -0,0 +1,74 @@
+<?php
+
+class Order extends DataObject {
+
+	private static $singular_name = 'Order';
+	private static $plural_name = 'Orders';
+	private static $description = 'Orders from FoxyCart Datafeed';
+
+	private static $db = array(
+        'Order_ID' => 'Int',
+        'Store_ID' => 'Int',
+        'StoreVersion' => 'Varchar',
+        'IsTest' => 'Boolean',
+        'IsHidden' => 'Boolean',
+        'DataIsFed' => 'Boolean',
+        'TransactionDate' => 'SS_Datetime',
+        'ProcessorResponse' => 'Varchar(200)',
+        'ShiptoShippingServiceDescription' => 'Varchar(200)',
+        'ProductTotal' => 'Currency',
+        'TaxTotal' => 'Currency',
+        'ShippingTotal' => 'Currency',
+        'OrderTotal' => 'Currency',
+        'PaymentGatewayType' => 'Varchar(100)',
+        'ReceiptURL' => 'Varchar(255)',
+        'OrderStatus' => 'Varchar(255)'
+    );
+
+	private static $has_one = array(
+        'Member' => 'Member'
+    );
+
+	private static $has_many = array();
+
+	private static $many_many = array(
+        'Products' => 'ProductPage'
+    );
+
+	private static $many_many_extraFields = array(
+        'Products' => array(
+            'Quantity' => 'Int'
+        )
+    );
+
+	private static $belongs_many_many = array();
+
+	private static $casting = array();
+	//private static $defaults = null;
+	//private static $default_sort = null;
+
+
+	private static $summary_fields = array();
+	private static $searchable_fields = array();
+	private static $field_labels = array();
+	private static $indexes = array();
+
+	public function getCMSFields(){
+		$fields = parent::getCMSFields();
+
+
+		$this->extend('updateCMSFields', $fields);
+		return $fields;
+	}
+
+	public function validate(){
+		$result = parent::validate();
+
+		/*if($this->Country == 'DE' && $this->Postcode && strlen($this->Postcode) != 5) {
+			$result->error('Need five digits for German postcodes');
+		}*/
+
+		return $result;
+	}
+
+}
