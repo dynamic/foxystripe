1b566b4697ff247101bd7b03b60a8e03a861928e
Jason Irish <jirish@dynamicagency.com>
2016-10-01 14:22:47 -0500

    ProductHolder - test catch up

---
+++ tests/ProductHolderTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class ProductHolderTest extends SapphireTest
+{
+    /**
+     * @var string
+     */
+    protected static $fixture_file = 'foxystripe/tests/FoxyStripeTest.yml';
+
+    /**
+     *
+     */
+    public function testGetCMSFields()
+    {
+        $object = singleton('ProductHolder');
+        $fields = $object->getCMSFields();
+        $this->assertInstanceOf('FieldList', $fields);
+
+        $object = $this->objFromFixture('ProductHolder', 'default');
+        $fields = $object->getCMSFields();
+        $this->assertInstanceOf('FieldList', $fields);
+    }
+
+    /**
+     *
+     */
+    public function testProducts()
+    {
+        $object = $this->objFromFixture('ProductHolder', 'default');
+        $this->assertInstanceOf('SS_List', $object->Products());
+        $this->assertEquals($object->Products(), $object->getManyManyComponents('Products')->sort('SortOrder'));
+    }
+}
