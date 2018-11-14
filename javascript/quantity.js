8c894a5dcaa18ff433c2db503522104d031a1073
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2018-11-14 10:23:00 -0600

    BUGFIX more flexible value fetching

--- javascript/quantity.js
+++ javascript/quantity.js
@@ -1,13 +1,14 @@
 ;(function ($) {
 	var field = $("input[name='x:visibleQuantity']"),
 		quantityField = $("input[name='quantity']"),
-		increment = $('.increase'),
-		decrement = $('.reduced'),
 		getLink = function (element) {
 			return element.parent().find("input[name='x:visibleQuantity']").data('link');
 		},
 		getCode = function (element) {
 			return element.parent().find("input[name='x:visibleQuantity']").data('code');
+		},
+		getId = function (element) {
+			return element.parent().find("input[name='x:visibleQuantity']").data('id');
 		},
 		disableSubmit = function (element) {
 			element.parent().parent().parent().find('.fs-add-to-cart-button').attr('disabled', true);
@@ -15,10 +16,11 @@
 		enableSubmit = function (element) {
 			element.parent().parent().parent().find('.fs-add-to-cart-button').attr('disabled', false);
 		},
-		queryNewValue = function (code, newValue, link, clicked) {
+		queryNewValue = function (code, newValue, link, id, clicked) {
 			var quantData = {
 				'code': code,
 				'value': newValue,
+				'id': id,
 				'isAjax': 1
 			};
 
@@ -26,30 +28,32 @@
 				type: 'get',
 				url: link + '?' + $.param(quantData),
 			}).done(function (response) {
-				quantityField.val(response);
+				clicked.parent().parent().parent().parent().find("input[name='quantity']").val(response);
 				enableSubmit(clicked);
 			}).fail(function (xhr) {
 				console.log('Error: ' + xhr.responseText);
 			});//*/
 		};
 
-	increment.on('click', function (event) {
-		var currentVal = field.val(),
+	$(document).on('click', 'button.increase', function (event) {
+		var visibleQuantity = $(this).parent().parent().find("input[name='x:visibleQuantity']"),
+			currentVal = visibleQuantity.val(),
 			newValue = parseInt(currentVal) + 1;
 
 		disableSubmit($(this));
-		queryNewValue(getCode($(this)), newValue, getLink($(this)), $(this));
-		field.val(newValue);
+		queryNewValue(getCode($(this)), newValue, getLink($(this)), getId($(this)), $(this));
+		visibleQuantity.val(newValue);
 	});
 
-	decrement.on('click', function (event) {
-		var currentVal = field.val(),
+	$(document).on('click', 'button.reduced', function (event) {
+		var visibleQuantity = $(this).parent().parent().find("input[name='x:visibleQuantity']"),
+			currentVal = visibleQuantity.val(),
 			newValue = parseInt(currentVal) - 1;
 
 		if (currentVal > 1) {
 			disableSubmit($(this));
-			queryNewValue(getCode($(this)), newValue, getLink($(this)), $(this));
-			field.val(newValue);
+			queryNewValue(getCode($(this)), newValue, getLink($(this)), getId($(this)), $(this));
+			visibleQuantity.val(newValue);
 		}
 	});
 }(jQuery));
